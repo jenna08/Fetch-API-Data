1687822e8cecd71dead387fbed27dbd234b367e9
Jennifer Fernandes <32226267+jenna08@users.noreply.github.com>
2021-04-06 22:50:10 +0530

    Updated GetUserServlet.java

--- GetUserServlet.java
+++ GetUserServlet.java
@@ -16,16 +16,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.json.JSONObject;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.client.RestTemplate;
 
-
-
-
-//@WebServlet("/GetUserServlet")
 public class GetUserServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
@@ -39,25 +30,6 @@ public class GetUserServlet extends HttpServlet {
 		response.getWriter().write(result);
 		//out.print(result);
 		//out.flush();
-		
-		// Method 1
-		/*
-		 * String userName = request.getParameter("userName").trim(); if(userName ==
-		 * null || "".equals(userName)){ userName = "Guest"; }
-		 * 
-		 * String greetings = "Hello " + userName;
-		 * 
-		 * response.setContentType("text/plain"); response.getWriter().write(greetings);
-		 */
-		 
-		
-		/* Method 2
-		 * List<String> list = new ArrayList<>(); list.add("item1"); list.add("item2");
-		 * list.add("item3"); String json = new Gson().toJson(list);
-		 * 
-		 * response.setContentType("application/json");
-		 * response.setCharacterEncoding("UTF-8"); response.getWriter().write(json);
-		 */
 	}
 
 	private String testMethod(HttpServletRequest request, HttpServletResponse response) {
@@ -68,46 +40,26 @@ public class GetUserServlet extends HttpServlet {
 		
 		try {
 			connection = new URL("https://reqres.in/api/users/2").openConnection();
-			//log.debug("url-> "+connection.getURL());
+			
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		connection.setRequestProperty("Accept", "application/json");
-		//log.debug("connection set");
+		
 		//Get Response  
 		try {
-			
 			reader = new BufferedReader(new InputStreamReader(connection.getInputStream(),"utf-8"));
-			//log.debug("reader-> "+reader);
-            String output = null;  
-            while ((output = reader.readLine()) != null) {  
-                strBuf.append(output);  
-            }
-            
-           
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
+			
+            		String output = null;  
+            		while ((output = reader.readLine()) != null) {  
+                		strBuf.append(output);  
+            		}
+            	} catch (IOException e) {
 			e.printStackTrace();
 		} 
 		
 		return strBuf.toString();
 	}
 
-	/*
-	 * private void getAccessToken() { HttpHeaders requestHeaders = new
-	 * HttpHeaders();
-	 * requestHeaders.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
-	 * requestHeaders.add("X-ENVIRONMENT",loginParam.getEnvironment()); String
-	 * plainClientCredentials = loginParam.getClientKey()+
-	 * ":"+loginParam.getSecretKey(); RestTemplate objRestTemplate = new
-	 * RestTemplate(); requestHeaders.add("Authorization","Basic " +
-	 * DatatypeConverter.printBase64Binary(plainClientCredentials.getBytes("UTF-8"))
-	 * ); HttpEntity<?> httpEntity = new HttpEntity<Object>(requestHeaders); String
-	 * endpointUrl=loginParam.getApiEndpoint()+"accesstoken";
-	 * //log.debug("Invite Endpoint URL :"+endpointUrl); ResponseEntity<Object>
-	 * response = objRestTemplate.exchange(endpointUrl, HttpMethod.GET,httpEntity,
-	 * Object.class); LinkedHashMap<String, Object> map = (LinkedHashMap<String,
-	 * Object>) response.getBody(); //return (String) map.get("accessToken"); }
-	 */
 }
